30d8ad9ed806fea9c4d60d086419c4fa8006f059
lucomoro <morelli.2000luca@gmail.com>
2023-06-28 15:21:02 +0200

    Add components related to database

--- client/src/components/TodoForm.jsx
+++ client/src/components/TodoForm.jsx
@@ -1,11 +1,19 @@
 import { useState } from 'react';
 
+import { useDispatch } from 'react-redux';
+
+import { AddNewTodo } from '../redux/actions';
+
 const TodoForm = () => {
 
-    const [text, setText] = useState("Hello");
+    const [text, setText] = useState("");
 
-    const onFormSubmit = () => {
+    const dispatch = useDispatch();
 
+    const onFormSubmit = (e) => {
+        e.preventDefault();
+
+        dispatch(AddNewTodo());
     }
 
     const onInputChange = (e) => {
