5255534f1285e81407a8c608793b25033e529f8b
lucomoro <morelli.2000luca@gmail.com>
2023-07-01 16:57:38 +0200

    Add change state

--- client/src/components/Todo.jsx
+++ client/src/components/Todo.jsx
@@ -1,16 +1,22 @@
+import { toggleTodo } from "../redux/actions";
+
+import { useDispatch } from 'react-redux';
 
 const Todo = ({ todo }) => {
+
+    const dispatch = useDispatch();
 
     return (
         <li
             className="task"
+            onClick={() => dispatch(toggleTodo(todo._id))}
         >
             <span>{todo.data}</span>
 
-            <span>
+            <span className="icon">
                 <i className="fas fa-trash"/>
             </span>
-            <span>
+            <span className="icon">
                 <i className="fas fa-pen"/>
             </span>
         </li>
