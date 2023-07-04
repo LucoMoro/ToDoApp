57023eeef720476cf4a58e4a6591fe68b4cecb80
lucomoro <morelli.2000luca@gmail.com>
2023-07-04 17:14:44 +0200

    Todo component refactor

--- client/src/components/Todo.jsx
+++ client/src/components/Todo.jsx
@@ -1,76 +1,89 @@
-import { useState, useMemo } from "react";
-
+import { useState, useEffect, useMemo } from "react";
 import { toggleTodo, updateTodo } from "../redux/actions";
 import { deleteTodo } from "../redux/actions";
-
 import { useDispatch } from 'react-redux';
 
 const Todo = ({ todo }) => {
+    const dispatch = useDispatch();
 
     const [editing, setEditing] = useState(false);
     const [text, setText] = useState(todo.data);
-    const textLower =  useMemo(() => todo.data.toLowerCase(), [todo.data]);
+    useEffect(() => {
+        setText(todo.data);
+    }, [todo.data]);
+    const todoTextLower = useMemo(() => todo.data.toLowerCase(), [todo.data]);
+    const goal = todo.data.split(':');
 
-    const dispatch = useDispatch();
+    const toggleTodoDone = () => {
+        dispatch(toggleTodo(todo._id));
+    };
 
     const onFormSubmit = (e) => {
         e.preventDefault();
-
-        setEditing(prevState => !prevState);
-
-        dispatch(updateTodo(todo._id, text))
+        if (editing) {
+            setEditing(false);
+            dispatch(updateTodo(todo._id, text));
+            if (todo.done) {
+                toggleTodoDone();
+            }            
+        }
     }
-
-    const goal = todo.data.split(':');
 
     return (
         <li
             className="task"
-            onClick={() => dispatch(toggleTodo(todo._id))}
+            onClick={toggleTodoDone}
             style={{
                 textDecoration: todo.done ? 'line-through' : '',
                 color: todo.done ? '#bdc3c7' : '#34495e'
             }}
-        >   
-        <span style ={{ display: editing ? 'none' : '' }}>
-            {(() => {
-                    if (textLower.includes('download')) {
-                        return <i class="fas fa-file-download"/>;
-                    } else if (textLower.includes('code') || textLower.includes('method') || textLower.includes('class')) {
-                        return <i class="fas fa-code"/>;
-                    } else if (textLower.includes('branch') || textLower.includes('github')) {
-                        return <i class="fas fa-code-branch"/>;
+        >
+            <span style={{ display: editing ? 'none' : '' }}>
+                {(() => {
+                    if (todoTextLower.includes('download')) {
+                        return <i class="fas fa-file-download" />;
+                    } else if (
+                        todoTextLower.includes('code')
+                        || todoTextLower.includes('method')
+                        || todoTextLower.includes('class')
+                    ) {
+                        return <i class="fas fa-code" />;
+                    } else if (
+                        todoTextLower.includes('branch')
+                        || todoTextLower.includes('github')
+                    ) {
+                        return <i class="fas fa-code-branch" />;
                     } else {
-                        return <i/>;
+                        return <i />;
                     }
                 })()}
-            <span></span> {goal[0]} <br/> {goal[1]}
-        </span>
+                <span></span> {goal[0]} <br /> {goal[1]}
+            </span>
 
             <form
-                style= {{ display: editing ? 'inline' : 'none' }}
+                style={{ display: editing ? 'inline' : 'none' }}
                 onSubmit={onFormSubmit}
             >
-                <input 
-                    type = "text" 
+                <input
+                    type="text"
                     value={text}
-                    className = "edit-todo"
+                    className="edit-todo"
                     onChange={(e) => {
                         setText(e.target.value);
-                    }} 
+                    }}
                 />
             </form>
-            <span className="icon" 
-                style={{color: todo.done ? '#008F39' : '#FFFFFF'}}
+            <span className="icon"
+                style={{ color: todo.done ? '#008F39' : '#FFFFFF' }}
             >
-                   <i class="fas fa-check-circle"></i>
+                <i class="fas fa-check-circle"></i>
             </span>
-            
-            <span className="icon" onClick = {() => dispatch(deleteTodo(todo._id))}>
-                <i className="fas fa-trash"/>
+
+            <span className="icon" onClick={() => dispatch(deleteTodo(todo._id))}>
+                <i className="fas fa-trash" />
             </span>
-            <span className="icon" onClick = {() => setEditing(prevState => !prevState)}>
-                <i className="fas fa-pen"/>
+            <span className="icon" onClick={() => setEditing(prevState => !prevState)}>
+                <i className="fas fa-pen" />
             </span>
         </li>
     )
